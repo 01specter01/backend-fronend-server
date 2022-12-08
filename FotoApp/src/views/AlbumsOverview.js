09a632cadeef3983b30bd7c4fb366c40420b60d7
zaid <zaedking445@gmail.com>
2022-12-08 21:37:09 +0100

    fak

--- FotoApp/src/views/AlbumsOverview.js
+++ FotoApp/src/views/AlbumsOverview.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 import { Container, Row, Col } from "react-bootstrap";
 import AlbumIcon from "../compontents/AlbumIcon";
 import AlbumNameForm from "../compontents/AlbumNameForm";
